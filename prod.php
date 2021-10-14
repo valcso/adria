4ce29e71139a854be498b63c877609bcdb982e05
valcso <52081901+valcso@users.noreply.github.com>
2021-10-14 15:45:49 +0200

    Update prod.php

--- prod.php
+++ prod.php
@@ -1,20 +1,15 @@
 <?php
 namespace App\Helpers;
 
-use App\Models\ProductFiles;
 use App\Models\Products;
-use App\Models\ProductsOther;
 use App\Objects\Product;
-use App\Objects\ProductOther;
-use Intervention\Image\Image;
 
-
-class ImporterOtherHelper
+class ImporterHelper
 {
 
     //Allowed tables for import/export
     //public $allowedTables = ['products', 'extrafields', 'product_category_glue', 'product_seo'];
-    public $allowedTables = ['products_other'];
+    public $allowedTables = ['products', 'extrafields', 'product_seo'];
 
 
     /**
@@ -32,29 +27,38 @@ class ImporterOtherHelper
     /**
      * EDIT products by importer
      * @param $request
-     * @return mixed
      */
     public function importProductsEdit($request)
     {
         $return;
         $importData = [];
+        
         $path = $this->getPath($request);
 
         \Excel::load($path, function ($reader) use (&$importData) {
 
             $items = $reader->get();
+//            dd($items);
             foreach ($items AS $item) {
-                $pcode = $item['products_other#product_code']; // Product code
-               
+                // Product code
+                $pcode = '';
+                if (isset($item['products#product_code'])) $pcode = $item['products#product_code']; 
+                if (isset($item['product_seo#product_code'])) $pcode = $item['product_seo#product_code'];
+                if (isset($item['product_code'])) $pcode = $item['product_code'];
                 if ($pcode == '') {
                     continue;
                 }
 
                 foreach ($item AS $key => $value) {
-                    $segments = explode('#', $key);
-                    $table = $segments[0];
-                    $field = @$segments[1];
-
+                    if(strpos($key, '#') !== false) {
+                        $segments = explode('#', $key);
+                        $table = $segments[0];
+                        $field = @$segments[1];
+                    }
+                    else {
+                        $table = 'products';
+                        $field = $key;
+                    }
                     if (in_array($table, $this->allowedTables)) {
                         $importData[$pcode][$table][$field] = str_replace('.0', '', $value);
                     }
@@ -64,36 +68,39 @@ class ImporterOtherHelper
 
         /* checking for new products */
         $importCais =  array_keys($importData);
-        $products = ProductsOther::select('product_code')->whereIn('product_code', $importCais)->get();
+        $products = Products::select('product_code')->whereIn('product_code', $importCais)->get();
         $productsCais = $products->pluck('product_code')->toArray();
         $result['diff'] = array_diff($importCais, $productsCais);
 
+       
         foreach ($importData AS $key => $data) {
-            $product = new ProductOther(true, ['product_code' => $key], false);
             $return = $key;
+            $product = new Product(true, ['product_code' => $key], false);
             /* create slug */
-//            if (isset($data['products_other']['product_name']) ) {
-//                $data['products_other']['slug'] = '';
-//            }
+            if (isset($data['products']['name'])) {
+                $data['products']['slug'] = '';
+            }
 
             foreach ($data AS $key => $items) {
                 /* key is a table name */
                 $key_method = '_' . $key;
                 $product->$key_method($data[$key]);
+                $table = $key;
             }
 
-            $getProduct= Product::select('name','ef_proizvodjac','ef_dezen')->where('product_code',$return)->first();
-            $descName = $getProduct->name;
-            $proizvodjac = $getProduct->ef_proizvodjac;
-            $dezen = $getProduct -> ef_dezen;
-    
-            app('App\Http\Controllers\Admin\ProductsController')->updateProductDescription($return,$descName);
-            app('App\Http\Controllers\Admin\ProductsController')->updateProductShortDesc($return,$proizvodjac,$dezen); 
-
+                $getProduct= Product::select('*')->where('product_code',$return)->first();
+                if(!empty($getProduct -> name) && !empty($getProduct -> ef_proizvodjac) && !empty($getProduct -> ef_dezen))
+                {
+                    $descName = $getProduct->name;
+                    $proizvodjac = $getProduct->ef_proizvodjac;
+                    $dezen = $getProduct -> ef_dezen;
+                    app('App\Http\Controllers\Admin\ProductsController')->updateProductDescription($return,$descName);
+                    app('App\Http\Controllers\Admin\ProductsController')->updateProductShortDesc($return,$proizvodjac,$dezen); 
+                }
             unset($product);
         }
 
-        return $return;
+        return $result;
     }
 
     /**
@@ -111,132 +118,63 @@ class ImporterOtherHelper
 
             $items = $reader->get();
             foreach ($items AS $item) {
-                $pcode = $item['products_other#product_code']; // Product code
+                $pcode = '';
+                if (isset($item['products#product_code'])) $pcode = $item['products#product_code']; 
+                if (isset($item['product_code'])) $pcode = $item['product_code'];
                 if ($pcode == '') {
                     continue;
                 }
-
                 foreach ($item AS $key => $value) {
-                    $segments = explode('#', $key);
-                    $table = $segments[0];
-                    $field = @$segments[1];
-
-                    if($key == "products_other#ef_kategorija"){
-                        $value = strtolower($value);
+                    if(strpos($key, '#') !== false) {
+                        $segments = explode('#', $key);
+                        $table = $segments[0];
+                        $field = @$segments[1];
                     }
-
-                    if($key == "products_other#product_group"){
-                        $value = strtolower($value);
+                    else {
+                        $table = 'products';
+                        $field = $key;
                     }
-
                     if (in_array($table, $this->allowedTables)) {
                         $importData[$pcode][$table][$field] = str_replace('.0', '', $value);
-                    }
-
-                    if($key == "products_other#image" && !is_null($value)){
-                        //$this->addImages($pcode, $value);
                     }
                 }
             }
         });
 
-        $products = ProductsOther::select('product_code')->whereIn('product_code', array_keys($importData))->get();
+        $products = Products::select('product_code')->whereIn('product_code', array_keys($importData))->get();
 
         if(!$products->isEmpty())
         {
             return ['all' => array_keys($importData), 'duplicated' => $products->toArray()];
         }
 
-
         foreach ($importData AS $key => $data) {
             $return = $key;
-            $product = new ProductOther(true, ['product_code' => $key], true);
+            $product = new Product(true, ['product_code' => $key], true);
+
             /* create slug */
-            if (isset($data['products_other']['product_name'])) {
-                $data['products_other']['slug'] = '';
+            if (isset($data['products']['name'])) {
+                $data['products']['slug'] = '';
             }
 
             foreach ($data AS $key => $items) {
                 /* key is a table name */
                 $key_method = '_' . $key;
-                // Product object update database
                 $product->$key_method($data[$key]);
             }
 
-        $getProduct= Product::select('name','ef_proizvodjac','ef_dezen')->where('product_code',$return)->first();
-        $descName = $getProduct->name;
-        $proizvodjac = $getProduct->ef_proizvodjac;
-        $dezen = $getProduct -> ef_dezen;
-
-        app('App\Http\Controllers\Admin\ProductsController')->updateProductDescription($return,$descName);
-        app('App\Http\Controllers\Admin\ProductsController')->updateProductShortDesc($return,$proizvodjac,$dezen);
-
-        unset($product);
-        
-    }
-        return $return;
-    }
-
-    public function addImages($cai, $links, $onlyNew = false){
-//        $path = 'https://i.stack.imgur.com/koFpQ.png';
-////        $filename = basename($path);
-//        $filename = $cai.'_'.rand(1,9999).time();
-//        Image::make($path)->save(public_path('images/' . $filename));
-
-        $path = public_path("products/" . $cai . '/');
-        if (!\File::exists($path)) {
-            \File::makeDirectory($path, 0775);
-        } else {
-            if ($onlyNew) {
-                return 0;
-            }
-        }
-
-        $path_thumbs = public_path("products/" . $cai . '/thumbs/');
-        if (!\File::exists($path_thumbs)) {
-            \File::makeDirectory($path_thumbs, 0775);
-        }
-
-        $allLinks = explode(',', $links);
-
-        if(count($links) > 4) {
-            $links = array_slice($allLinks, 0, 4);
-        } else {
-            $links = $allLinks;
-        }
-
-
-        foreach ($links AS $link){
-            //$image = \Image::make($link);
-            if(strlen($link) < 20) { continue; }
-            $link = str_replace(' ', '', str_replace(',', '',$link) );
-            //$extension =  'jpg'; //$image->getClientOriginalExtension();
-
-
-            //$filename = $cai . '_' . rand(1, 99999) . time() . '.' . $extension;
-            $buff = explode('/', $link);
-            $filename = end($buff);
-
-            $testFile = @file_get_contents($link);
-
-                if($testFile) {
-                    \Image::make($link)->save($path . $filename);
-                    $image = \Image::make($path . $filename);
-
-                    $entry = new ProductFiles();
-                    //$entry->mime = $image->getClientMimeType();
-                    //$entry->original_filename = $image->getClientOriginalName();
-                    $entry->filename = $filename;
-                    $entry->product_code = $cai;
-                    $entry->save();
-
-                    \Image::make($path . $filename)->resize(250, null, function ($constraint) {
-                        $constraint->aspectRatio();
-                    })->save($path_thumbs . '' . $filename);
-
-                    unset($entry);
+            $getProduct= Product::select('*')->where('product_code',$return)->first();
+                if(!empty($getProduct -> name) && !empty($getProduct -> ef_proizvodjac) && !empty($getProduct -> ef_dezen))
+                {
+                    $descName = $getProduct->name;
+                    $proizvodjac = $getProduct->ef_proizvodjac;
+                    $dezen = $getProduct -> ef_dezen;
+            
+                    app('App\Http\Controllers\Admin\ProductsController')->updateProductDescription($return,$descName);
+                    app('App\Http\Controllers\Admin\ProductsController')->updateProductShortDesc($return,$proizvodjac,$dezen); 
                 }
-            }
-            return 1;
+
+            unset($product);
+        }
     }
 }
